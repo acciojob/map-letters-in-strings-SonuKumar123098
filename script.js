55d29fe9bdaa7e2966716e1b8c8df6b946d79621
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-02-03 13:34:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function mapLetters(params) {
+	let obj={};
+	if(!params || params.lemgth===0) return obj;
+	
+	for(let index in params){
+		obj[params[index]] ? obj[params[index]].push(index) : (obj.params[index]=[index]);
+	}
+	return obj;
+}
+
+// let str=prompt('enter the word or line');
+// alert(obj.stringigy(mapLetters(str)));
